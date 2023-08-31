10721e2cf821e8ee56437baa14da4eb517f263e2
Nifri <nifri@protonmail.com>
2023-08-31 17:08:23 +0200

    made the code readable my lord what was i looking at (sorry - i dont wanna be mean)

---
+++ cmd/b_field.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func BFieldHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	dat_path := "/home/potato/Documents/sensorsave.dat"
+	img_path := b_plot(dat_path)
+	f, err := os.Open(img_path)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: "Here's the local B-field of my room:",
+			Files: []*discordgo.File{
+				&discordgo.File{
+					Name:   img_path,
+					Reader: f,
+				},
+			},
+		},
+	})
+}
---
+++ cmd/test.go
@@ -0,0 +1,12 @@
+package cmd
+
+import "github.com/bwmarrin/discordgo"
+
+func TestHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: "Test!",
+		},
+	})
+}
